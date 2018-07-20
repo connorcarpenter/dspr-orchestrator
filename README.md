65a2c678dd915fdf733b427aaca87d85c4aa0bc4
connorcarpenter <connorcarpenter@gmail.com>
2018-07-20 12:05:50 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# dspr-orchestrator
+#dspr-orchestrator
 
-dspr-orchestrator
+# Install uWebSockets-0.13.0 and dependencies
+RUN apt-get install -y libuv1-dev libssl-dev
+RUN wget https://github.com/uWebSockets/uWebSockets/archive/v0.14.4.tar.gz -O uws.tar.gz && \
+    tar xvfz uws.tar.gz && \
+    cd uWebSockets-0.14.4 && \
+    make && \
+    make install && \
+    cd ../../ && \
+    rm -r uWebSockets-0.14.4 && \
+    rm uws.tar.gz && \
+    ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <uWS/uWS.h>
+#include <iostream>
+
+int main() {
+    uWS::Hub h;
+
+    std::string noResponse = "no response";
+    std::string bffResponse = "ws://localhost:3171";
+
+    h.onHttpRequest([&](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes)
+    {
+        if (req.headers->keyLength == 3 && strncmp(req.headers->key, "get", 3) == 0)
+        {
+            //request is a GET request
+            if (req.headers->valueLength == 17 && strncmp(req.headers->value, "/orchestrator/bff", 17) == 0)
+            {
+                res->end(bffResponse.data(), bffResponse.length());
+                return;
+            }
+        }
+
+        res->end(noResponse.data(), noResponse.length());
+    });
+
+    h.getDefaultGroup<uWS::SERVER>().startAutoPing(30000);
+    if (h.listen("localhost", 3171)) {
+        std::cout << "Listening to port 3171" << std::endl;
+    } else {
+        std::cerr << "Failed to listen to port" << std::endl;
+        return -1;
+    }
+
+    h.run();
+}
